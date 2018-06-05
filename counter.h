b516cce637fbe72a5747663687ea5f1bb3e5f627
Alexey <darknessest>
2018-06-05 13:49:42 +0800

    RC2 Added "calculator" feature And operator>> was implemented

--- counter.h
+++ counter.h
@@ -14,39 +14,41 @@ class counter {
 
     template<typename TT>
     friend std::ostream &operator<<(std::ostream &out, const counter<TT> &);
+    template<typename TT>
+    friend std::istream &operator>>(std::istream &in, counter<TT> &);
 
     counter<T> &operator=(const counter<T> &);
-    template<typename U>
-    counter<T> &operator=(const U &);
+    template<typename UU>
+    counter<T> &operator=(const UU &);
 
     //  +
     template<typename TT, typename BB>
     friend counter<TT> operator+(const counter<TT> &, const counter<BB> &);
     template<typename UU>
     counter<T> inline operator+(const UU &);
-    template<typename TT, typename U>
-    friend inline U operator+(const U &, const counter<TT> &);
+    template<typename TT, typename UU>
+    friend inline UU operator+(const UU &, const counter<TT> &);
     //  -
     template<typename TT, typename BB>
     friend counter<TT> operator-(const counter<TT> &, const counter<BB> &);
     template<typename UU>
     counter<T> inline operator-(const UU &);
-    template<typename TT, typename U>
-    friend inline U operator-(const U &, const counter<TT> &);
+    template<typename TT, typename UU>
+    friend inline UU operator-(const UU &, const counter<TT> &);
     //  *
     template<typename TT, typename BB>
     friend counter<TT> operator*(const counter<TT> &, const counter<BB> &);
     template<typename UU>
     counter<T> inline operator*(const UU &);
-    template<typename TT, typename U>
-    friend inline U operator*(const U &, const counter<TT> &);
+    template<typename TT, typename UU>
+    friend inline UU operator*(const UU &, const counter<TT> &);
     //  /
     template<typename TT, typename BB>
     friend counter<TT> operator/(const counter<TT> &, const counter<BB> &);
     template<typename UU>
     counter<T> inline operator/(const UU &);
-    template<typename TT, typename U>
-    friend inline U operator/(const U &, const counter<TT> &);
+    template<typename TT, typename UU>
+    friend inline UU operator/(const UU &, const counter<TT> &);
 
     //  ==
     template<typename TT, typename BB>
@@ -94,7 +96,7 @@ class counter {
     inline double sin() { return std::sin(this->val*PI/180); }
     inline double cos() { return std::cos(this->val*PI/180); }
     inline double tan() { return std::tan(this->val*PI/180); }
-    inline double exp() { return std::exp(this->val*PI/180); }
+    inline double exp() { return std::exp(this->val); }
     template<typename U>
     inline auto pow(const U &x) { return std::pow(this->val, x); }
 };
@@ -104,11 +106,15 @@ std::ostream &operator<<(std::ostream &out, const counter<T> &a) {
     out << a.val;
     return out;
 }
-
+template<typename T>
+std::istream &operator>>(std::istream &in, counter<T> &a) {
+    in >> a.val;
+    return in;
+}
 //  +
 template<typename T, typename B>
-counter<T> operator+(const counter<T> &a, const counter<B> &b) {
-    return counter<T>(a.val + b.val);
+counter<T> operator+(const counter<T> &a, const counter<B> &rhs) {
+    return counter<T>(a.val + rhs.val);
 }
 template<typename T>
 template<typename U>
@@ -121,8 +127,8 @@ U inline operator+(const U &x, const counter<TT> &rhs) {
 }
 //  -
 template<typename T, typename B>
-counter<T> operator-(const counter<T> &a, const counter<B> &b) {
-    return counter<T>(a.val - b.val);
+counter<T> operator-(const counter<T> &a, const counter<B> &rhs) {
+    return counter<T>(a.val - rhs.val);
 }
 template<typename T>
 template<typename U>
@@ -135,8 +141,8 @@ U inline operator-(const U &x, const counter<TT> &rhs) {
 }
 //  *
 template<typename T, typename B>
-counter<T> operator*(const counter<T> &a, const counter<B> &b) {
-    return counter<T>(a.val*b.val);
+counter<T> operator*(const counter<T> &a, const counter<B> &rhs) {
+    return counter<T>(a.val*rhs.val);
 }
 template<typename T>
 template<typename U>
@@ -149,8 +155,8 @@ U inline operator*(const U &x, const counter<TT> &rhs) {
 }
 //  /
 template<typename T, typename B>
-counter<T> operator/(const counter<T> &a, const counter<B> &b) {
-    return counter<T>(a.val/b.val);
+counter<T> operator/(const counter<T> &a, const counter<B> &rhs) {
+    return counter<T>(a.val/rhs.val);
 }
 template<typename T>
 template<typename U>
@@ -255,78 +261,3 @@ counter<T> &counter<T>::operator=(const U &x) {
     val = x;
     return *this;
 }
-
-
-
-
-//#include <iostream>
-//
-//using namespace std;
-//
-//template<class A>
-//class counter {
-// protected:
-//    long integer;
-//    double fraction;
-// public:
-//    counter<A>();
-//    explicit counter<A>(const A &x);
-//    counter<A> &operator+=(const counter<A> &another);
-//    template<class U>
-//    counter<A> operator+(const counter<U> &another) {
-//        counter<A> temp;
-//        temp.integer = this->integer + another.integer;
-//        temp.fraction = this->fraction + another.fraction;
-//        return temp;
-//    }
-//
-////    template <typename TT,typename BB>
-////    friend counter<TT> operator +(const counter<TT>&, const counter<BB>&);
-//
-////    double operator-(counter const &x) {
-//////        return val - x.val;
-////    }
-////    double operator/(counter const &x) {
-//////        return val/x.val;
-////    }
-////    double operator*(counter const &x) {
-//////        return val*x.val;
-////    }
-//    friend ostream &operator<<(ostream &out, const counter &a);
-////    friend istream &operator>>(istream &in, T &a);
-//};
-//template<class A>
-//counter<A>::counter():integer(0), fraction(0.0) {}
-//template<class A>
-//counter<A>::counter(const A &x):integer((long) x), fraction(x - integer) {
-//    if (fraction < 0)
-//        fraction *= -1;
-//}
-//
-//template<class A>
-//counter<A> &counter<A>::operator+=(const counter<A> &another) {
-//    this->fraction += another.fraction;
-//    this->integer += another.integer;
-//    return *this;
-//}
-//
-//
-//
-////template<typename A>
-////ostream &operator<<(ostream &out, const counter<A> &a) {
-////    out << a.integer + a.fraction;
-////    return out;
-////}
-////
-////template<class A>
-////counter<A> operator-(const counter<A> &a, const counter<A> &b) {
-////    counter<A> temp;
-////    temp.integer = a.integer - b.integer;
-////    temp.fraction = (a.fraction - b.fraction) > 0 ? a.fraction - b.fraction : (1 + a.fraction - b.fraction);//integer--
-////    return temp;
-////}
-//
-//template
-//class counter<long>;
-//template
-//class counter<double>;
--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include "counter.h"
 
 int main() {
-    counter<double> c;
-    counter a{5};
-    counter b{2};
-    c = 5;
-    std::cout << c << std::endl;
-
+    counter<double> a;
+    counter<double> b;
+    while (std::cin >> a >> b)
+        std::cout << "a+b = " << a + b << "\t b+a = " << b + a << std::endl
+                  << "a-b = " << a - b << "\t b-a = " << b - a << std::endl
+                  << "a*b = " << a*b << "\t b-a = " << b*a << std::endl
+                  << "\n(deg)" << std::endl
+                  << "sin(a) = " << a.sin() << "\t sin(b) = " << b.sin() << std::endl
+                  << "cos(a) = " << a.cos() << "\t cos(b) = " << b.cos() << std::endl
+                  << "tan(a) = " << a.tan() << "\t tan(b) = " << b.tan() << std::endl
+                  << std::endl;
 }
